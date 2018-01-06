65f2aa08d7b5b56ec0cf2e4e0de65a3dc89641c3
Arifin Firdaus <arifinfrds@ekas-MacBook-Pro.local>
2018-01-06 11:47:00 +0700

    done add initial project

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Arifin Firdaus on 1/6/18.
+//  Copyright © 2018 Arifin Firdaus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+    }
+
+}
+
